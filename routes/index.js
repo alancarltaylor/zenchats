ba4a56f69bd2abde87c7494472a432598f77f602
Alan Taylor <alancarltaylor@gmail.com>
2016-03-18 16:12:56 -0600

    second commit

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/rooms', function(req, res, next) {
+  //go to the views directory
+  //find a file named views
+  //run it through the template engine (handlebars)
+  //stick it in the layout
+  //send it back to the browser
+  res.render('room', {
+    strawberry: req.body.roomName
+  });
+  // res.send(req.body.roomName.toUpperCase())
+});
 
 module.exports = router;
